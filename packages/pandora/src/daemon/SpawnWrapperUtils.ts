2920fa29d7806474078cf35e8f92e55ca3f62baa
Harry Chen <czy88840616@gmail.com>
2017-11-09 20:17:29 +0800

    fix: something wrong with ignore ts file

--- packages/pandora/src/daemon/SpawnWrapperUtils.ts
+++ packages/pandora/src/daemon/SpawnWrapperUtils.ts
@@ -1,14 +1,10 @@
 import wrap = require('spawn-wrap');
-import {GlobalConfigProcessor} from '../universal/GlobalConfigProcessor';
 const wrapFile = require.resolve('./spawnWrapper');
 
 export class SpawnWrapperUtils {
   static wrapped = false;
   static wrap() {
     if(!SpawnWrapperUtils.wrapped) {
-      const globalConfigProcessor = GlobalConfigProcessor.getInstance();
-      const globalConfig = globalConfigProcessor.getAllProperties();
-      process.env.__pandora_hook = globalConfig['hooks'];
       wrap([wrapFile]);
     }
   }
--- packages/pandora/src/daemon/spawnWrapper.ts
+++ packages/pandora/src/daemon/spawnWrapper.ts
@@ -1,6 +1,10 @@
 'use strict';
+import {GlobalConfigProcessor} from '../universal/GlobalConfigProcessor';
 const wrap = require('spawn-wrap');
-const patch = process.env.__pandora_hook;
+const globalConfigProcessor = GlobalConfigProcessor.getInstance();
+const globalConfig = globalConfigProcessor.getAllProperties();
+const patch = process.env.__pandora_hook = globalConfig['hook'];
+
 if (patch) {
   require(patch);
 }
