800eae4274d91dbbd9e814614e92bab5544c2d47
Harry Chen <czy88840616@gmail.com>
2017-11-14 14:44:14 +0800

    test: fix port

--- packages/pandora/test/daemon/Monitor.test.ts
+++ packages/pandora/test/daemon/Monitor.test.ts
@@ -25,14 +25,14 @@ describe('Monitor', function () {
 
   it('should start() be ok', async () => {
     await monitor.start();
-    const ret = await urllib.request('http://127.0.0.1:8006/');
+    const ret = await urllib.request('http://127.0.0.1:7002/');
     expect(ret.res.data.toString()).to.be.includes('restful service');
   });
 
   it('should stop() be ok', async () => {
     await monitor.stop();
     try {
-      await urllib.request('http://127.0.0.1:8006/');
+      await urllib.request('http://127.0.0.1:7002/');
     } catch (err) {
       return;
     }
