9482da470bcf8e80a9e5a5780c6fc0dcea3b010b
Harry Chen <czy88840616@gmail.com>
2017-11-29 11:40:04 +0800

    v1.0.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,4 +2,4 @@ echo ">>>>>> copy readme to pandora/readme"
 cp README.md ./package/pandora/README.md
 source `dirname $0`/build.sh
 git add .
-lerna publish $* --conventional-commits
+lerna publish $*
