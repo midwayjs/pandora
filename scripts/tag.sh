031cd2c16402622e1ae08b40fcda954467573149
Harry Chen <czy88840616@gmail.com>
2017-11-30 18:00:23 +0800

    chore: update tag script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -9,7 +9,7 @@ do
       cd $pkg
       echo ">>>>>> Tag Clean"
       echo ">>>>>>" $pkg
-      SHH="npm dist-tag add `node -e "console.log(require('./package.json').name)"`@$1 $2"
+      SHH="npm dist-tag add `node -e "console.log(require('./package.json').name)"`@`node -e "console.log(require('./package.json').version) $1"
       echo $SHH
       $SHH
     fi
