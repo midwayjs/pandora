0c2efdeb61b45341c047dc5489f6a0795839b2e4
Harry Chen <czy88840616@gmail.com>
2017-11-30 11:59:37 +0800

    chore: demo for get application info

--- packages/metrics/src/endpoint/impl/InfoEndPoint.ts
+++ packages/metrics/src/endpoint/impl/InfoEndPoint.ts
@@ -2,4 +2,14 @@ import {EndPoint} from '../EndPoint';
 
 export class InfoEndPoint extends EndPoint {
   group: string = 'info';
+
+  processQueryResults(results) {
+    let allResults = super.processQueryResults(results);
+    let apps = DaemonBootstrapUtil.getLatestDaemonInstance().listApplication();
+    return allResults.map((result) => {
+      result.reboot = apps[result['appName']].reboot;
+      return result;
+    });
+  }
+
 }
