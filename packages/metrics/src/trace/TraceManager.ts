6067702b6f43044cd457b9ad015d8565e9f49715
MarioDu <dujiakun@gmail.com>
2017-12-14 18:21:55 +0800

    fix: catch error when ns missing

--- packages/metrics/src/trace/TraceManager.ts
+++ packages/metrics/src/trace/TraceManager.ts
@@ -58,16 +58,21 @@ export class TraceManager {
   }
 
   create(options) {
-    options.traceId = options.traceId || uuid();
-    const traceId = options.traceId;
-    this.ns.set(TRACEID, traceId);
-    options.ns = this.ns;
-    const tracer = new Tracer(options);
-    this.traceContainer[traceId] = tracer;
-    (<any>tracer).once('finish', () => {
-      this.removeTracer(traceId);
-    });
-    return tracer;
+    try {
+      options.traceId = options.traceId || uuid();
+      const traceId = options.traceId;
+      this.ns.set(TRACEID, traceId);
+      options.ns = this.ns;
+      const tracer = new Tracer(options);
+      this.traceContainer[traceId] = tracer;
+      (<any>tracer).once('finish', () => {
+        this.removeTracer(traceId);
+      });
+      return tracer;
+    } catch (error) {
+      console.log('create trace error.', error);
+      return null;
+    }
   }
 
   removeTracer(traceId) {
