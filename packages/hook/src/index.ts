93b48ed1111e404b899f1f70099cccc73b7c1ab5
MarioDu <dujiakun@gmail.com>
2017-12-26 15:08:26 +0800

    feat: export http client shimmer

--- packages/hook/src/index.ts
+++ packages/hook/src/index.ts
@@ -7,4 +7,5 @@ export { BluebirdPatcher } from './patch/BlueBird';
 export { HttpServerPatcher } from './patch/HttpServer';
 export { MySQLPatcher } from './patch/MySQL';
 export { MySQLShimmer } from './patch/shimmers/mysql/Shimmer';
+export { HttpClientShimmer } from './patch/shimmers/http-client/Shimmer';
 export { extractPath } from './utils/Utils';
