84fdc0dd7acc283f521d5be64d661dde0a7c14a5
MarioDu <dujiakun@gmail.com>
2017-12-27 23:06:53 +0800

    fix: export hook constants

--- packages/hook/src/index.ts
+++ packages/hook/src/index.ts
@@ -10,3 +10,10 @@ export { MySQL2Patcher } from './patch/MySQL2';
 export { MySQLShimmer } from './patch/shimmers/mysql/Shimmer';
 export { HttpClientShimmer } from './patch/shimmers/http-client/Shimmer';
 export { extractPath } from './utils/Utils';
+export {
+  INSTANCE_UNKNOWN as DB_INSTANCE_UNKNOWN,
+  HOST_UNKNOWN as DB_HOST_UNKNOWN,
+  TABLE_UNKNOWN as DB_TABLE_UNKNOWN,
+  DEFAULT_HOST as HTTP_DEFAULT_HOST,
+  DEFAULT_PORT as HTTP_DEFAULT_PORT
+} from './utils/Constants';
