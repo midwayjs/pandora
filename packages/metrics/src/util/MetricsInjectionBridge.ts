92b8116ee6c082beb0677f961881a20a3d227076
GuangWong <guangwong@gmail.com>
2018-01-16 15:29:53 +0800

    feat: add type annotation

--- packages/metrics/src/util/MetricsInjectionBridge.ts
+++ packages/metrics/src/util/MetricsInjectionBridge.ts
@@ -1,11 +1,12 @@
 import {MetricsManager} from '../common/MetricsManager';
+import {Facade} from 'pandora-hub';
 const assert = require('assert');
 
 export class MetricsInjectionBridge {
 
   // FIXME: import {Daemon} from 'pandora'; will occurs a cycle deps
   static daemon: any = null;
-  static ipcHub: any = null;
+  static ipcHub: Facade = null;
   static metricsManager;
 
   static getMetricsManager(): MetricsManager {
@@ -27,7 +28,7 @@ export class MetricsInjectionBridge {
     return this.daemon;
   }
 
-  static setIPCHub(ipcHub) {
+  static setIPCHub(ipcHub: Facade) {
     this.ipcHub = ipcHub;
   }
 
