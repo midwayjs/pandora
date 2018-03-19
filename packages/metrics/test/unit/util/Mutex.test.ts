e78ab4be4be2d34f2fcb5cfad6483436ee94414f
kurten <chinkurten@gmail.com>
2018-03-19 15:20:45 +0800

    fix: waiting not clear bug

---
+++ packages/metrics/test/unit/util/Mutex.test.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { Mutex } from '../../../src/util/Mutex';
+
+
+describe('/test/unit/util/Mutex.test.ts', () => {
+  it('test lock with no await use', async () => {
+    const mutex = new Mutex();
+    expect(mutex.tryLock(3000)).true;
+    expect(mutex.tryLock(3000)).false;
+
+    await new Promise(resolve => {
+      mutex.wait(resolve);
+    });
+
+    await new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    });
+
+    expect(mutex.waitingCount).eq(0);
+  });
+});
