da4093b938a4b40e7ee0663307f071aae549c064
Mario Du <dujiakun@gmail.com>
2018-12-14 11:22:20 +0800

    fix: fix hub test

--- packages/hub/test/hub/Hub.test.ts
+++ packages/hub/test/hub/Hub.test.ts
@@ -34,7 +34,7 @@ describe('Hub', function () {
       const hub = new Hub();
       await hub.start();
       await new Promise(resolve => (<any> hub).messengerServer.close(resolve));
-      await expect(hub.stop()).to.be.rejectedWith('not running');
+      await expect(hub.stop()).to.be.rejectedWith(/not running/i);
     });
 
   });
