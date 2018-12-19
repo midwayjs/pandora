47aa4d02cd8f0544f00de4f005f55d462f8d10ea
GuangWong <guangwong@gmail.com>
2018-12-19 19:29:33 +0800

    test: add test cases for component-decorator

---
+++ packages/component-decorator/test/ComponentReflector.test.ts
@@ -0,0 +1,25 @@
+import {ComponentReflector, componentName, componentConfig, dependencies} from '../src/ComponentReflector';
+import {expect} from 'chai';
+describe('ComponentReflector', function () {
+  it('should decorating componentName be ok', () => {
+    const expectName = 'expectName';
+    @componentName(expectName)
+    class TestClass {
+    }
+    expect(ComponentReflector.getComponentName(TestClass)).to.be.equal(expectName);
+  });
+  it('should decorating componentConfig be ok', () => {
+    const expectConfig = {a: 1};
+    @componentConfig(expectConfig)
+    class TestClass {
+    }
+    expect(ComponentReflector.getComponentConfig(TestClass)).to.be.equal(expectConfig);
+  });
+  it('should decorating dependencies be ok', () => {
+    const expectDependencies = [1, 2, 3];
+    @dependencies(expectDependencies)
+    class TestClass {
+    }
+    expect(ComponentReflector.getDependencies(TestClass)).to.be.equal(expectDependencies);
+  });
+});
